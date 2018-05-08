5b39ef9e12d130bc085b859cca9dc0c083e582ae
valentinab25 <valentinab25>
2018-05-08 13:43:08 +0200

    Added script | Refs #94505

---
+++ run-clair-scanner.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+CLAIR_URL=${CLAIR_URL:-"http://clair:6060"}
+LEVEL=${LEVEL:-"Critical"}
+image="0"
+host=$(curl -s  http://rancher-metadata/latest/self/host/hostname)
+environment_uuid=$(curl -s  http://rancher-metadata/latest/self/host/environment_uuid)
+
+for cont in $(docker ps --format '{{.Image}} {{.Names}}')
+do
+if [[ "$image" == "0" ]]; then
+   image=$cont
+   continue
+else
+   name=$cont
+   #echo "Am citit $image $name"
+   if [ $(echo "$name" | grep -c "^r-") -eq 0 ]; then
+      container=$name;
+   else
+       container=$(echo $name | awk -F "-" '{ final=$2; for (i=3;i<NF;i++) { final=final"-"$i};  print final} ')
+   fi
+   #echo $container
+   stack=$(curl -s http://rancher-metadata/latest/containers/$container/stack_name)
+   if [ -z "$environment_name" ]; then
+      environment_name=$(curl -s http://rancher-metadata/latest/stacks/$stack/environment_name)
+   fi
+
+   service=$(curl -s  http://rancher-metadata/latest/containers/$container/labels/io.rancher.stack_service.name)
+   TMPDIR=`pwd` clair-scanner --ip=`hostname` --clair=$CLAIR_URL -t=$LEVEL --all=false  $image 1>/tmp/scan_result  2>&1
+   if [ $? -eq 0 ]; then
+     echo "{\"environment_name\": \"$environment_name\", \"environment_uuid\": \"$environment_uuid\", \"hostname\": \"$host\", \"stack\": \"$stack\", \"service\": \"$service\", \"container\": \"$container\", \"image\": \"$image\", \"status\": \"OK\", \"result\": \"$(tail -n 2 /tmp/scan_result | tr '\n' ';' | sed 's/;/\\n/g' )\"}"
+
+   else
+    echo "{\"environment_name\": \"$environment_name\", \"environment_uuid\": \"$environment_uuid\", \"hostname\": \"$host\", \"stack\": \"$stack\", \"service\": \"$service\", \"container\": \"$container\", \"image\": \"$image\", \"status\": \"ERROR\", \"result\": \"$(cat /tmp/scan_result | tr '\n' ';' | sed 's/;/\\n/g'  )\"}"
+
+   fi
+   # reset image for next loop
+   image="0"
+fi
+done
+
